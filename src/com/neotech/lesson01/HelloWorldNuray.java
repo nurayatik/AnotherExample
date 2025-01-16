493421396fe6f448e391df6491ae1556d762ee23
nuray <nuray310322@gmail.com>
2025-01-16 09:29:05 -0500

    first commit

---
+++ src/com/neotech/lesson01/HelloWorldNuray.java
@@ -0,0 +1,22 @@
+package com.neotech.lesson01;
+
+public class HelloWorldNuray { // this marks the beginning of the class
+
+	// to save changes: CTRL + S
+
+	// the main method is the starting point to run a java application
+	public static void main(String[] args) { // this is the beginning of the method
+
+	// CTRL + SHIFT + F --> formats the code automatically
+
+	System.out.println("Hello World!"); // the first instruction we are writing
+
+	System.out.println("          How are you doing today???");
+
+	}// this is the end of the method
+	
+	// this is the end of the java class
+	
+	}
+
+
